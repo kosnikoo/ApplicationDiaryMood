de08f12b5e19035a0910065ab7a3335d1424cc21
Костоусов Никита Александрович <KostousovNA@dom201.local>
2025-11-12 12:16:06 +0700

    add

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace DiaryMood
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
